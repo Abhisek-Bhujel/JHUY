9e3d6ce77b7c7cc8960d8f2761629c5039608309
Abhisek Bhujel <bhujelabhisek0@gmail.com>
2024-02-22 19:35:05 -0500

    Initial Commit

---
+++ home/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, "home/index.html")
+def post(request,pk):
+    return render(request, "home/post.html")
